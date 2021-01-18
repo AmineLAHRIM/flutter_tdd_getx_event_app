0d87717bdcfd93507d995b004f03a81955d97c46
aminelahrim <aminelahrimdev@gmail.com>
2021-01-18 02:17:22 +0100

    Some Unit Testing v2 ✅

--- lib/config/di/register_module.dart
+++ lib/config/di/register_module.dart
@@ -1,13 +1,16 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 @module
 abstract class RegisterModule {
   @singleton
   DataConnectionChecker get dataConnectionChecker;
 
+  @singleton
+  http.Client get client => http.Client();
+
   @singleton // if you need to pre resolve the value
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
-
 }
